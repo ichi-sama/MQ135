9098d1bbb95ab18d245c32989bf339c32ad7d118
tempura-boy <47944113+ichi-sama@users.noreply.github.com>
2024-05-27 15:54:00 +0900

    Update MQ135.h

--- MQ135.h
+++ MQ135.h
@@ -1,6 +1,6 @@
 /**************************************************************************/
 /*!
-@file     MQ135.h
+@file     MQ135.cpp
 @author   G.Krocker (Mad Frog Labs)
 @license  GNU GPLv3
 
@@ -13,42 +13,126 @@ the datasheet but the information there seems to be wrong.
 v1.0 - First release
 */
 /**************************************************************************/
-#ifndef MQ135_H
-#define MQ135_H
 
-#include "Arduino.h"
+#include "MQ135.h"
 
-/// The load resistance on the board
-//#define RLOAD 10.0
-/// Calibration resistance at atmospheric CO2 level
-//#define RZERO 76.63
-/// Parameters for calculating ppm of CO2 from sensor resistance
-#define PARA 116.6020682
-#define PARB 2.769034857
+/**************************************************************************/
+/*!
+@brief  Default constructor
 
-/// Parameters to model temperature and humidity dependence
-#define CORA 0.00035
-#define CORB 0.02718
-#define CORC 1.39538
-#define CORD 0.0018
+@param[in] pin  The analog input pin for the readout of the sensor
+*/
+/**************************************************************************/
 
-/// Atmospheric CO2 level for calibration purposes
-//#define ATMOCO2 397.13
+MQ135::MQ135(uint8_t pin, float RLOAD, float RZERO, float ATMOCO2) {
+  _pin = pin;
+  _RLOAD = RLOAD;
+  _RZERO = RZERO;
+  _ATMOCO2 = ATMOCO2;
+}
 
-class MQ135 {
- private:
-  uint8_t _pin;
-  float _RLOAD, _RZERO, _ATMOCO2;
 
- public:
-  MQ135(uint8_t pin, float RLOAD, float RZERO, float ATMOCO2);
-  float getCorrectionFactor(float t, float h);
-  float getResistance();
-  float getResistanceESP32();
-  float getCorrectedResistance(float t, float h);
-  float getPPM();
-  float getCorrectedPPM(float t, float h);
-  float getRZero();
-  float getCorrectedRZero(float t, float h);
-};
-#endif
+/**************************************************************************/
+/*!
+@brief  Get the correction factor to correct for temperature and humidity
+
+@param[in] t  The ambient air temperature
+@param[in] h  The relative humidity
+
+@return The calculated correction factor
+*/
+/**************************************************************************/
+float MQ135::getCorrectionFactor(float t, float h) {
+  return CORA * t * t - CORB * t + CORC - (h-33.)*CORD;
+}
+
+/**************************************************************************/
+/*!
+@brief  Get the resistance of the sensor, ie. the measurement value
+
+@return The sensor resistance in kOhm
+*/
+/**************************************************************************/
+float MQ135::getResistance() {
+  int val = analogRead(_pin);
+  return ((1023.0/(float)val) * 5 - 1.)*_RLOAD;
+}
+
+/**************************************************************************/
+/*!
+@brief  ESP32 Get the resistance of the sensor, ie. the measurement value
+
+@return The sensor resistance in kOhm
+*/
+/**************************************************************************/
+float MQ135::getResistanceESP32() {
+  int val = analogRead(_pin);
+  return ((4095.0/(float)val) * (5.0/3.3) - 1.)*_RLOAD;
+}
+
+/**************************************************************************/
+/*!
+@brief  Get the resistance of the sensor, ie. the measurement value corrected
+        for temp/hum
+
+@param[in] t  The ambient air temperature
+@param[in] h  The relative humidity
+
+@return The corrected sensor resistance kOhm
+*/
+/**************************************************************************/
+float MQ135::getCorrectedResistance(float t, float h) {
+  return getResistance()/getCorrectionFactor(t, h);
+}
+
+/**************************************************************************/
+/*!
+@brief  Get the ppm of CO2 sensed (assuming only CO2 in the air)
+
+@return The ppm of CO2 in the air
+*/
+/**************************************************************************/
+float MQ135::getPPM() {
+  return PARA * pow((getResistance()/_RZERO), -PARB);
+}
+
+/**************************************************************************/
+/*!
+@brief  Get the ppm of CO2 sensed (assuming only CO2 in the air), corrected
+        for temp/hum
+
+@param[in] t  The ambient air temperature
+@param[in] h  The relative humidity
+
+@return The ppm of CO2 in the air
+*/
+/**************************************************************************/
+float MQ135::getCorrectedPPM(float t, float h) {
+  return PARA * pow((getCorrectedResistance(t, h)/_RZERO), -PARB);
+}
+
+/**************************************************************************/
+/*!
+@brief  Get the resistance RZero of the sensor for calibration purposes
+
+@return The sensor resistance RZero in kOhm
+*/
+/**************************************************************************/
+float MQ135::getRZero() {
+  return getResistance() * pow((_ATMOCO2/PARA), (1./PARB));
+}
+
+/**************************************************************************/
+/*!
+@brief  Get the corrected resistance RZero of the sensor for calibration
+        purposes
+
+@param[in] t  The ambient air temperature
+@param[in] h  The relative humidity
+
+@return The corrected sensor resistance RZero in kOhm
+*/
+/**************************************************************************/
+float MQ135::getCorrectedRZero(float t, float h) {
+  return getCorrectedResistance(t, h) * pow((_ATMOCO2/PARA), (1./PARB));
+}
