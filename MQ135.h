f921f66483e154cf0d6c4988fcf816ecb8d08fcd
tempura-boy <47944113+ichi-sama@users.noreply.github.com>
2024-05-27 15:50:08 +0900

    Update MQ135.h

--- MQ135.h
+++ MQ135.h
@@ -15,16 +15,13 @@ v1.0 - First release
 /**************************************************************************/
 #ifndef MQ135_H
 #define MQ135_H
-#if ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
+
+#include "Arduino.h"
 
 /// The load resistance on the board
-#define RLOAD 10.0
+//#define RLOAD 10.0
 /// Calibration resistance at atmospheric CO2 level
-#define RZERO 76.63
+//#define RZERO 76.63
 /// Parameters for calculating ppm of CO2 from sensor resistance
 #define PARA 116.6020682
 #define PARB 2.769034857
@@ -36,16 +33,18 @@ v1.0 - First release
 #define CORD 0.0018
 
 /// Atmospheric CO2 level for calibration purposes
-#define ATMOCO2 397.13
+//#define ATMOCO2 397.13
 
 class MQ135 {
  private:
   uint8_t _pin;
+  float _RLOAD, _RZERO, _ATMOCO2;
 
  public:
-  MQ135(uint8_t pin);
+  MQ135(uint8_t pin, float RLOAD, float RZERO, float ATMOCO2);
   float getCorrectionFactor(float t, float h);
   float getResistance();
+  float getResistanceESP32();
   float getCorrectedResistance(float t, float h);
   float getPPM();
   float getCorrectedPPM(float t, float h);
